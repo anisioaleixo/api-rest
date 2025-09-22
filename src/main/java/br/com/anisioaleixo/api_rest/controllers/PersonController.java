f69f353aedaf1c9c55e45730bf2e1a43bdc5599e
Anisio Aleixo <anisioaleixo@yahoo.com.br>
2025-09-21 22:37:48 -0300

    Creating findAll endpoint for Person

--- src/main/java/br/com/anisioaleixo/api_rest/controllers/PersonController.java
+++ src/main/java/br/com/anisioaleixo/api_rest/controllers/PersonController.java
@@ -8,14 +8,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
     @Autowired
     private PersonServices services;
 
+    @GetMapping(produces = "application/json")
+    public List<Person> findAll() {
+        return services.findAll();
+    }
+
     @GetMapping(value = "/{id}", produces = "application/json")
     public Person findById(@PathVariable String id) {
         return services.findById(id);
     }
+
 }
--- src/main/java/br/com/anisioaleixo/api_rest/services/PersonServices.java
+++ src/main/java/br/com/anisioaleixo/api_rest/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.anisioaleixo.api_rest.services;
 import br.com.anisioaleixo.api_rest.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,16 @@ public class PersonServices {
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll() {
+        logger.info("Finding all people!");
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
     public Person findById(String id) {
         logger.info("Finding one person!");
 
@@ -22,6 +34,16 @@ public class PersonServices {
         person.setLastName("Aleixo");
         person.setAddress("Belém - PA - Brasil");
         person.setGender("Male");
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstNamee " + i);
+        person.setLastName("LastName " + i);
+        person.setAddress("Some address in Brasil " + i);
+        person.setGender((i % 2 == 0) ? "Male" : "Female");
         return person;
     }
 }
