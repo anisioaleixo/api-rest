f62c344c816da1c928186645c4d231a2afe85cda
Anisio Aleixo <anisioaleixo@yahoo.com.br>
2025-09-30 19:33:05 -0300

    Adding migrate create table person and populate person

---
+++ src/main/resources/db/migration/V2__populate_person.sql
@@ -0,0 +1,6 @@
+INSERT INTO person (address,first_name,gender,last_name) VALUES
+	 ('Belém - PA - Brasil','Anisio','Male','Aleixo Teixeira Junior'),
+	 ('Belém - PA - Brasil','Anisio2','Male','Aleixo2'),
+	 ('Belém - PA - Brasil','Anisio3','Male','Aleixo3'),
+	 ('Belém - PA - Brasil','Anisio4','Male','Aleixo4'),
+	 ('Belém - PA - Brasil','Anisio5','Male','Aleixo5');
