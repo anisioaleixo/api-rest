8076d9fe5c059b19fb1df2a367f8a5afd25f1c53
Anisio Aleixo <anisioaleixo@yahoo.com.br>
2025-10-02 21:39:53 -0300

    Implementing Content Negotiation via QUERY Parameter, using MediaType json and xml

--- src/main/java/br/com/anisioaleixo/api_rest/controllers/PersonController.java
+++ src/main/java/br/com/anisioaleixo/api_rest/controllers/PersonController.java
@@ -3,9 +3,11 @@ package br.com.anisioaleixo.api_rest.controllers;
 import br.com.anisioaleixo.api_rest.data.DTO.PersonDTO;
 import br.com.anisioaleixo.api_rest.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -15,25 +17,39 @@ public class PersonController {
     @Autowired
     private PersonServices services;
 
-    @GetMapping(produces = "application/json")
+    @GetMapping(produces = {
+                MediaType.APPLICATION_JSON_VALUE,
+                MediaType.APPLICATION_XML_VALUE})
     public List<PersonDTO> findAll() {
         return services.findAll();
     }
 
-    @GetMapping(value = "/{id}", produces = "application/json")
+    @GetMapping(value = "/{id}", produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE})
     public PersonDTO findById(@PathVariable Long id) {
         var person = services.findById(id);
         person.setBirthDay(new Date());
         return person;
     }
 
-    @PostMapping(produces = "application/json", consumes = "application/json")
+    @PostMapping(produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE},
+            consumes = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<PersonDTO> created(@RequestBody PersonDTO person) {
         PersonDTO createdPerson = services.created(person);
         return ResponseEntity.status(201).body(createdPerson);
     }
 
-    @PutMapping(produces = "application/json", consumes = "application/json")
+    @PutMapping(produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE},
+            consumes = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE})
     public PersonDTO update(@RequestBody PersonDTO person) {
         return services.update(person);
     }
